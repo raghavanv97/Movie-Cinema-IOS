9d91abde811f6c3e021653dd28dbc160b077fdc0
vijayraghavan <chennai1997>
2020-06-19 11:41:23 +0530

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by vijay raghavan on 19/06/20.
+//  Copyright © 2020 vijay raghavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
